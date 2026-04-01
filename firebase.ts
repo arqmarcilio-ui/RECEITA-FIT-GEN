6f958a34a8c51dea1c4c32454bd53c2878aa69ec
arqmarcilio-ui <arqmarcilio@gmail.com>
2026-04-01 13:48:41 -0300

    Update firebase.ts

--- firebase.ts
+++ firebase.ts
@@ -3,7 +3,15 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
 import { getFirestore, doc, onSnapshot, getDocFromServer, collection, addDoc, query, where, orderBy, limit, getDocs, updateDoc, deleteDoc, serverTimestamp } from 'firebase/firestore';
 import { getStorage, ref, uploadString, getDownloadURL } from 'firebase/storage';
-import firebaseConfig from './firebase-applet-config.json';
+const firebaseConfig = {
+  apiKey: "AIzaSyB3ahZb8ww9FQs-ZGKm8ooXy6l5gjGeiDE",
+  authDomain: "receita-fit-gen-2.firebaseapp.com",
+  projectId: "receita-fit-gen-2",
+  storageBucket: "receita-fit-gen-2.firebasestorage.app",
+  messagingSenderId: "941794526294",
+  appId: "1:941794526294:web:633f7b0815839d023be7f7",
+  measurementId: "G-QSBXY4K7PH"
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
