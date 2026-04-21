52affccbd74cbfac63a51c1fdb033abdb6d16f61
arqmarcilio-ui <arqmarcilio@gmail.com>
2026-04-21 11:26:29 -0300

    Update StepForm.tsx

--- components/StepForm.tsx
+++ components/StepForm.tsx
@@ -311,7 +311,7 @@ const StepForm: React.FC<StepFormProps> = ({ initialData, onSubmit, onCancel, la
                 <div className="space-y-4">
                   <h3 className="text-2xl font-black text-slate-900 uppercase leading-none tracking-tight">{t.availableIngredients}</h3>
                   <textarea 
-                    rows={4}
+                    rows={3}
                     placeholder={t.ingredientsPlaceholder}
                     className="w-full p-5 bg-white border-2 border-slate-300 rounded-[2rem] text-xs font-bold text-slate-900 placeholder:text-slate-300 focus:border-emerald-500 focus:outline-none transition-all resize-none"
                     value={formData.ingredients}
@@ -322,7 +322,7 @@ const StepForm: React.FC<StepFormProps> = ({ initialData, onSubmit, onCancel, la
                 <div className="space-y-4">
                   <h3 className="text-2xl font-black text-slate-900 uppercase leading-none tracking-tight">{t.avoidIngredients}</h3>
                   <textarea 
-                    rows={3}
+                    rows={2}
                     placeholder={t.avoidPlaceholder}
                     className="w-full p-5 bg-white border-2 border-slate-300 rounded-[2rem] text-xs font-bold text-slate-900 placeholder:text-slate-300 focus:border-emerald-500 focus:outline-none transition-all resize-none"
                     value={formData.dispensableIngredients}
